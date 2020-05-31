d9ba7f1b8c4dfe4cf95b69c3ccdd2f8db0ac1e81
MOHAMMAD SONJI <mohammadsonji360@gmail.com>
2020-05-31 14:52:01 +0300

    addproduct page will redirects to pagestore again

--- addproduct.php
+++ addproduct.php
@@ -7,7 +7,8 @@ $quantity=$_POST['quantity'];
 $product_id=$_POST['submit'];
 $query="INSERT INTO shoppingcart(customer_id,product_ID,quantiy) VALUES ('$id','$product_id','$quantity') ";
 $result= mysqli_query($db, $query) or die ("Couldnt execute query.");
-echo "Product added to shopping cart";
+// echo "Product added to shopping cart";
+include('pagestore.php');
 }
 else{
   require('login.php');
